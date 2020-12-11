ad4c22c40285b3daf089c2d0f3872a84d1522968
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-12-10 19:11:26 -0600

    Actualización

--- JS/1.js
+++ JS/1.js
@@ -1,5 +1,23 @@
-const txt = "Hola Mundo"
-function count(txt) {
+//! ##############################################  Ejercicio 1 #######################################################
+/*function count(txt) {
     a = txt.length
     console.log(a)
-} count(txt);
+} count(txt);*/
+//! ##############################################  Ejercicio 2 #######################################################
+/* var txt = "Hola Mundo";
+function r(txt) {
+    console.log(txt.slice(0, 4));
+} r(txt) */
+//! ##############################################  Ejercicio 3 #######################################################
+/*var txt = "Hola que tal";
+function r(txt) {
+    console.log(txt.split(" ",))
+}
+r(txt)*/
+//! ##############################################  Ejercicio 4 #######################################################
+/* let txt = "Hola que tal";
+function r(txt) {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i + " " + txt)
+    }
+} r(txt) */
